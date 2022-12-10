8c7de11313aec2a81f85dd6915d47cf1db177b9d
Shane Anthony <s.anthony743@canterbury.ac.uk>
2022-12-10 14:42:31 +0000

    Passed all 16 tests

--- server/server.js
+++ server/server.js
@@ -3,57 +3,34 @@ const express = require('express')
 const app = express()
 const port = 8000
 const cors = require('cors')
+const path = require('path')
 
 app.use(express.json());
 app.use(cors())
 
-let ITEM = {
-  1: {
-    "id": 1,
-    "user_id": "user1",
-    "keywords": ["hammer", "nails", "tools"],
-    "description": "A hammer and nails set",
-    "image": "https://www.placekitten.com/200/100",
-    "lat": 1.10,
-    "lon": 1.20,
-    "date_from": "2022-11-22T08:22:39.067408",
-    "date_to": "2022-11-22T08:22:39.067408"
-  },
-  2: {
-    "id":2,
-    "user_id": "user2",
-    "keywords": ["hammer", "nails", "tools"],
-    "description": "A hammer and nails set",
-    "image": "https://www.placekitten.com/200/100",
-    "lat": 1.10,
-    "lon": 1.20,
-    "date_from": "2022-11-22T08:22:39.067408",
-    "date_to": "2022-11-22T08:22:39.067408"
-}}
+let item = {}
 
 //Routing ↓
 
 // GET
 
 app.get('/', (req, res) => {
-  res.status(200).send('<html><body>Hello World</body></html>')
-
+  return res.status(200).send('<html><body>Hello World</body></html>')
 })
 
 app.get('/items/', (req, res) => {
   if( req.query.user_id)
   {
-    res.status(200).json(Object.values(ITEM).filter(o  => o.user_id == req.query.user_id))
-    res.send(ITEM)
+    res.status(200).json(Object.values(item).filter(o  => o.user_id == req.query.user_id))
     return;
   }
-  res.status(200).json(Object.values(ITEM))
+  res.status(200).json(Object.values(item))
 })
 
 app.get('/item/:id', (req,res) => {
-  if(Object.keys(ITEM).includes(req.params.id))
+  if(Object.keys(item).includes(req.params.id))
   {
-    res.status(200).json(ITEM[req.params.id])
+    res.status(200).json(item[req.params.id])
   }
   else
   {
@@ -66,9 +43,12 @@ app.get('/item/:id', (req,res) => {
 app.post('/item', (req, res) => { // https://www.tutorialspoint.com/expressjs/expressjs_restful_apis.htm
 
   req.body.id=newId;
-  var newId=  Math.max( ...Object.keys(ITEM)) +1;
-
-  ITEM[newId] = {
+  var newId=  Math.max( ...Object.keys(item)) +1;
+  if (newId == -Infinity){
+    newId = 0
+  }
+  
+  item[newId] = {
     id: newId,
     user_id: req.body.user_id,
     keywords: req.body.keywords,
@@ -85,7 +65,7 @@ app.post('/item', (req, res) => { // https://www.tutorialspoint.com/expressjs/ex
     return res.status(405).send("Some fields may be empty.")
   }
   else {
-    res.status(201).json(ITEM[newId])
+    res.status(201).json(item[newId])
   }
 
 })
@@ -93,9 +73,9 @@ app.post('/item', (req, res) => { // https://www.tutorialspoint.com/expressjs/ex
 // DELETE
 
 app.delete('/item/:id', (req,res) => {
-  if (Object.keys(ITEM).includes(req.params.id))
+  if (Object.keys(item).includes(req.params.id))
   {
-    delete(ITEM[req.params.id]);
+    delete(item[req.params.id]);
     res.status(204).send("This item no longer exists.");
   } 
   else 
@@ -104,6 +84,7 @@ app.delete('/item/:id', (req,res) => {
   }
   
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
